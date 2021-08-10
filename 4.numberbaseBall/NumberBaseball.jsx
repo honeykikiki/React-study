26111da69167ce7aa7f04d464df12519342a8a6b
honeykikiki <tjdwls75666@naver.com>
2021-08-10 16:27:02 +0900

    setTimeout

--- 4.numberbaseBall/NumberBaseball.jsx
+++ 4.numberbaseBall/NumberBaseball.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { PureComponent, createRef } from "react";
 import Try from "./Try";
 
 // const NumberBaseball = () => {};
@@ -16,32 +17,36 @@ function getNumber() {
   return array;
 }
 
-class NumberBaseball extends Component {
+class NumberBaseball extends PureComponent {
   state = {
     result: "",
     value: "",
     answer: getNumber(),
     tries: [],
   };
+  shouldComponentUpdate() {}
 
   onSubmitForm = (e) => {
+    const { result, value, tries, answer } = this.state;
     e.preventDefault();
-    if (this.state.value === this.state.answer.join("")) {
-      this.setState({
-        result: "홈런",
-        tries: [
-          ...this.state.tries,
-          {
-            try: this.state.value,
-            result: "홈런",
-          },
-        ],
+    if (value === answer.join("")) {
+      this.setState((prevState) => {
+        return {
+          result: "홈런",
+          tries: [
+            ...prevstate.tries,
+            {
+              try: value,
+              result: "홈런",
+            },
+          ],
+        };
       });
     } else {
-      const answerArray = this.state.value.split("").map((v) => parseInt(v));
+      const answerArray = value.split("").map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
+      if (tries.length >= 9) {
         this.setState({
           result: `10번 넘게 틀려서 실패 답은 : ${answer.join(",")}였습니다 `,
         });
@@ -51,25 +56,27 @@ class NumberBaseball extends Component {
           answer: getNumber(),
           tries: [],
         });
+        this.inputRef.current.focus();
       } else {
-        console.log(answerArray);
-        console.log(this.state.answer);
         for (let i = 0; i < 4; i++) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [
-            ...this.state.tries,
-            {
-              try: this.state.value,
-              result: `${strike} 스트라이크 ${ball} 볼입니다`,
-            },
-          ],
-          value: "",
+        this.setState((prevState) => {
+          this.inputRef.current.focus();
+          return {
+            tries: [
+              ...prevState.tries,
+              {
+                try: value,
+                result: `${strike} 스트라이크 ${ball} 볼입니다`,
+              },
+            ],
+            value: "",
+          };
         });
       }
     }
@@ -81,21 +88,25 @@ class NumberBaseball extends Component {
     });
   };
 
+  inputRef = createRef();
+
   render() {
+    const { result, value, tries } = this.state;
     return (
       <>
-        <h1>{this.state.result}</h1>
+        <h1>{result}</h1>
         <form onSubmit={this.onSubmitForm}>
           <input
+            ref={this.inputRef}
             maxLength={4}
-            value={this.state.value}
+            value={value}
             onChange={this.onChangInput}
             type="number"
           />
         </form>
-        <div>시도 : {this.state.tries.length} </div>
+        <div>시도 : {tries.length} </div>
         <ul>
-          {this.state.tries.map((v, i) => {
+          {tries.map((v, i) => {
             return <Try key={`${i + 1}차 시도 :`} tryInfo={v} />;
           })}
         </ul>
--- 4.numberbaseBall/Try.jsx
+++ 4.numberbaseBall/Try.jsx
@@ -1,14 +1,45 @@
-import React, { Component } from "react";
+// import React, { Component, memo } from "react";
+import React, { PureComponent, memo, useState } from "react";
 
-class Try extends Component {
+class Try extends PureComponent {
+  // shouldComponentUpdate(nextProps, nextState, nextContext) {
+  //   if (this.props.tryInfo.try == nextState) {
+  //     return true;
+  //   }
+  //   false;
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: "this.props.result",
+      try: this.props.try,
+    };
+  }
+
   render() {
+    const { tryInfo } = this.props;
     return (
       <li>
-        <div>{this.props.tryInfo.try}</div>
-        <div>{this.props.tryInfo.result}</div>
+        <div>{tryInfo.try}</div>
+        <div>{tryInfo.result}</div>
+        <div>{this.state.result}</div>
       </li>
     );
   }
 }
+
+// const Try = memo(({ tryInfo }) => {
+//   const [result, setResult] = useState(tryInfo.result);
+
+//   const onClick = () => {
+//     setResult("1");
+//   };
+//   return (
+//     <li>
+//       <div>{tryInfo.try}</div>
+//       <div onClick={onClick}>{result}</div>
+//     </li>
+//   );
+// });
 
 export default Try;
