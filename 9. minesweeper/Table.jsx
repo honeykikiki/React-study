0ae916fdea95a2e1319f43dc5f71e4656d4a182c
honeykikiki <tjdwls75666@naver.com>
2021-08-17 15:42:14 +0900

    commit

---
+++ 9. minesweeper/Table.jsx
@@ -0,0 +1,20 @@
+import React, { useContext, useState } from "react";
+import Tr from "./Tr";
+import { TableContext } from "./MineSearch";
+
+const Table = () => {
+  const { tableData } = useContext(TableContext);
+  return (
+    <>
+      <table>
+        {Array(tableData.length)
+          .fill()
+          .map((tr, i) => (
+            <Tr rowIndex={i} />
+          ))}
+      </table>
+    </>
+  );
+};
+
+export default Table;
